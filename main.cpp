d563636c4b9f01864a5be6239a4ed4c6d4f50d2a
Taythir <109971377+Taythir@users.noreply.github.com>
2022-09-22 23:29:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include "backwardChaining.h"
+#include "backwardChaining.cpp"
 
+using namespace std;
 int main()
 {
-   struct carParts
-   {  
-     bool broken = false;
-     string name;
-   };
    
-   carParts problem = backwardChaining(); // problem will be the carPart that is broken. 
+   carParts problem;
+   problem = backwardChaining();  // problem will be the carPart that is broken. 
                                           // problem will be what is passed to the forward chaining function
                                           // if problem.name == "No Problem Found" then a broken part wasn't found
    return 0;
