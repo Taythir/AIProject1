65c6f1219831e1632ddbed35df7928a0c95a5557
Taythir <109971377+Taythir@users.noreply.github.com>
2022-09-22 23:28:56 -0500

    Update backwardChaining.h

--- backwardChaining.h
+++ backwardChaining.h
@@ -1,4 +1,12 @@
 #ifndef BACKWARDCHAINING_H
 #define BACKWARDCHAINING_H
-void backwardChaining();
+#include <iostream>
+
+using namespace std;
+    struct carParts
+    {
+        string name;
+        bool broken;
+    };
+carParts backwardChaining();
 #endif
